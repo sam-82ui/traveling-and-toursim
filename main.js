527e089b9cdcedb199e43f63f6dd2e846e7fd7e6
unknown <samtomy776@gmail.com>
2025-03-18 22:09:04 +0630

    travel

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-
     $('.fa-bars').click(function() {
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
 
-    $(window).on('looad scrool',function() {
+    $(window).on('looad scroll',function() {
 
         $('.fa-bars').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
@@ -16,5 +15,15 @@ $(document).ready(function() {
             $('header').removeClass('header-active');
         }
 
+        $('section').each(function() {
+            var id = $(this).attr('id');
+            var height = $(this).height();
+            var offset = $(this).offset().top - 200;
+            var top = $(window).scrollTop();
+            if(top > offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find('[data-scorll="' + id + '"]').addClass('active');
+            }
+        }); 
     });
 });
